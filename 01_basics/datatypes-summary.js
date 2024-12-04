d7c039d30762f9d00ec8fe8f1bd2ab7de9dde734
Narendra Singh <137052399+Narendra-Singh-Devda@users.noreply.github.com>
2024-12-04 09:28:11 +0000

    Memory allocation in stack and heap

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -35,3 +35,32 @@ const myFunction = function(){
 console.log(typeof myObj);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+// ===========================================
+
+// Memory allocation in stack and heap
+
+
+// Stack (Primitive),   Heap (Non-Primitive)
+
+
+let myYoutubename = "hiteshchoudarydotcom"
+
+let anothername = myYoutubename;
+anothername = "chaiaurcode"
+console.log(myYoutubename);
+console.log(anothername);
+
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne;
+
+userTwo.email = "nikku@gmail.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
